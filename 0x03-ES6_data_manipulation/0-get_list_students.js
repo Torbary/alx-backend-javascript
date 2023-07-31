68d1aab633ba77f8387c8485e62d09f7e7257731
Torbary <torbarey020@gmail.com>
2023-07-31 22:35:06 +0000

    comment and eslint requiremrnt fixed

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,12 @@
 /**
-	* Get a lists of students.
-	* @author Abdullah Ismail <https://github.com/Torbary>
-	* @returns a key value array
-	*/
+ * Retrieves a list of students.
+ * @author Ismail Abdullah <https://github.com/Torbary>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
-	return [
-	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	{ id: 2, firstName: 'James', location: 'Columbia' },
-	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
- ];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
