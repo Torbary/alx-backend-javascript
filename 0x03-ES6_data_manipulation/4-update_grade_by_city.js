96e799e6ee370f1cb720a5d136e2a4e3644e1d91
Torbary <torbarey020@gmail.com>
2023-08-01 16:28:35 +0000

    whitespace removed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(studentsList, city, newGrades) 
   return studentsList
 		.filter(student => student.location === city)
 		.map(student => {
-				const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-				return {
-				  ...student,
-					grade: matchingGrade ? matchingGrade.grade : 'N/A',
-				};
-			});
+			const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
+			return {
+			  ...student,
+				grade: matchingGrade ? matchingGrade.grade : 'N/A',
+			};
+		});
 }
