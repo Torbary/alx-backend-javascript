f32cc38139f11f85ba8609f70e21cc50766a6f43
Torbary <torbarey020@gmail.com>
2023-08-01 16:32:44 +0000

    bracekt added to filter function:

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
   return studentsList
-		.filter(student => student.location === city)
+		.filter((student) => student.location === city)
 		.map(student => {
 			const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
 			return {
