140a4dc0255d4de73221eac4dd84e39c6bc3ef9b
Torbary <torbarey020@gmail.com>
2023-08-01 21:51:41 +0000

    modified

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,3 @@
-/**
-	* Retrives ids from a list of students
-	* @param {{
-	*   id: NUmber,
-	*   firstName: string,
-	*   location: string
-	* }[]} students - The list of students
-	* @author Torbary <https://github.com/Torbary>
-	* @returns
-	*/
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
 		return students.map((student) => student.id);
