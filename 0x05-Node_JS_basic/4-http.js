30745606518ac67009b5475a919cbb36d3a11ac4
Torbary <torbarey020@gmail.com>
2023-09-18 23:21:40 +0100

    modified

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,14 @@
 const http = require('http');
-
-// Create an HTTP server
-const app = http.createServer((req, res) => {
-  // Set the response content type to plain text
+ 
+const hostname = '127.0.0.1';
+const port = 1245;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  // Send "Hello Holberton School!" as the response
   res.end('Hello Holberton School!\n');
 });
-
-// Listen on port 1245
-app.listen(1245);
-
-module.exports = app;
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
