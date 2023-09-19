d6d743312e324efb4caf90f808287e670780d8f8
Torbary <torbarey020@gmail.com>
2023-09-20 00:03:30 +0100

    code modified

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,57 +1,45 @@
 const fs = require('fs');
 
-
-const countStudents = (path) => {
-  // Attempt to read the database file synchronously
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-
-    if (lines.length <= 1) {
-      throw new Error('Database is empty');
-    }
-
-    const header = lines[0].split(',');
-    const fieldIndexMap = {};
-
-    // Find the index of the 'field' column
-    for (let i = 0; i < header.length; i++) {
-      if (header[i] === 'field') {
-        fieldIndexMap['field'] = i;
-        break;
-      }
-    };
-
-    if (!fieldIndexMap.hasOwnProperty('field')) {
-      throw new Error('Invalid database format: Missing "field" column');
-    };
-
-    const counts = {};
-    const lists = {};
-
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
-      const firstName = values[0];
-      const field = values[fieldIndexMap.field];
-
-      if (!counts[field]) {
-        counts[field] = 0;
-        lists[field] = [];
-      };
-
-      counts[field]++;
-      lists[field].push(firstName);
-    };
-
-    console.log(`Number of students: ${lines.length - 1}`);
-
-    for (const field in counts) {
-      if (counts.hasOwnProperty(field)) {
-        console.log(`Number of students in ${field}: ${counts[field]}. List: ${lists[field].join(', ')}`);
-      }
-    }
-  } catch (error) {
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const fileLines = fs
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
 };
 
